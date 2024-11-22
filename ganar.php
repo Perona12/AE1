dc60e09270175301778dae5a5e36394fa1bec7a7
Perona12 <alpebe@alumnatflorida.es>
2024-11-22 15:43:46 +0100

    Añadida página de victoria con botón para reiniciar el juego

---
+++ ganar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>¡Ganaste!</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+    <h1>¡Felicidades!</h1>
+    <p>¡Has adivinado la palabra correctamente!</p>
+    <form method="post" action="index.php">
+        <button type="submit">Jugar de nuevo</button>
+    </form>
+</body>
+
+</html>
